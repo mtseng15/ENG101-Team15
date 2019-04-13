2e2c5230ce2404df9953775b6a0d6bb0bf3d3f7e
mtseng15 <tseng.micah@gmail.com>
2019-04-13 10:09:08 -0500

    Seperating functions into lights and sound?

---
+++ magic_audio.py
@@ -0,0 +1,16 @@
+# Functions needed for Team 15's Magic 8 ball
+
+
+import subprocess
+import time
+import random
+
+
+
+
+def open_question():
+    print("What is the quesion you would like answered? Then press the button for my answer.")
+
+
+def answer():
+    print("My answer is yes")
---
+++ temp.py
@@ -0,0 +1,1 @@
+from magic_fun import *answer
